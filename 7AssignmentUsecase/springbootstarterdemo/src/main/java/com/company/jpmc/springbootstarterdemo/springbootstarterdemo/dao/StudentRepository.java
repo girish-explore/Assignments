9136a46a7c480e94cd34f6d26e3360d3c57dd9d4
girish-explore <77339278+girish-explore@users.noreply.github.com>
2022-06-03 12:43:03 +0530

    Delete 7AssignmentUsecase/springbootstarterdemo directory

--- 7AssignmentUsecase/springbootstarterdemo/src/main/java/com/company/jpmc/springbootstarterdemo/springbootstarterdemo/dao/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.company.jpmc.springbootstarterdemo.springbootstarterdemo.dao;
-
-import com.company.jpmc.springbootstarterdemo.springbootstarterdemo.dto.Student;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer>{
-    
-}
--- 7AssignmentUsecase/springbootstarterdemo/src/main/java/com/company/jpmc/springbootstarterdemo/springbootstarterdemo/service/StudentServiceInterface.java
+++
@@ -1,8 +0,0 @@
-package com.company.jpmc.springbootstarterdemo.springbootstarterdemo.service;
-
-import com.company.jpmc.springbootstarterdemo.springbootstarterdemo.dto.Student;
-
-public interface StudentServiceInterface {
-    
-    boolean save(Student student) throws Exception;
-}
