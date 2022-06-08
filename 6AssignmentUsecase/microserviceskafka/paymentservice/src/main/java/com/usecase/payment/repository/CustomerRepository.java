0c32a86440253403ad99e8bddcce0091987905c1
girish-explore <77339278+girish-explore@users.noreply.github.com>
2022-06-08 22:56:10 +0530

    Delete 6AssignmentUsecase/microserviceskafka directory

--- 6AssignmentUsecase/microserviceskafka/paymentservice/src/main/java/com/usecase/payment/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.usecase.payment.repository;
-
-import com.usecase.payment.domain.Customer;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-}
--- 6AssignmentUsecase/microserviceskafka/stockservice/src/main/java/com/usecase/stock/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.usecase.stock.repository;
-
-import com.usecase.stock.domain.Product;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-}
